ac8d47753eafa158cf9111bcb842a9c0d3a61432
Nullreff <git@nullreff.net>
2024-01-16 06:16:57 -0800

    Add test bulk endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 use axum::{
-    routing::get,
+    routing::{get, post},
+    extract::{
+        Extension,
+        Path,
+        Json,
+    },
+    http::{HeaderMap},
     Router,
 };
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/:org/:repo/objects/batch", post(objects_batch));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let addr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("Listening on {}", addr);
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn root() -> &'static str {
     "Hubless is runnng"
+}
+
+async fn objects_batch(
+    headers: HeaderMap,
+    Path((org, repo)): Path<(String, String)>,
+    Json(body): Json<serde_json::Value>
+) {
+    println!("Got batch request for {}/{}", org, repo);
+    println!("Body: {}", body);
+    println!("Headers: {:?}", headers);
 }
